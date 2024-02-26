848a834f0e32d4bd66c8d28cec0a01820191e7ae
Hemanth Nag Chopparapu <139227551+hemu312004@users.noreply.github.com>
2024-02-26 12:42:23 +0530

    Add files via upload

---
+++ DAY 24 OF 50 Sequence-Detector_Moore/Sequence_Detector.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 26.02.2024 11:49:16
+// Design Name: 
+// Module Name: Sequence_Detector
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Sequence_Detector(
+
+    );
+endmodule
