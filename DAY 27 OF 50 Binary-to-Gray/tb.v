7c6a479a232cbeb5e24d06944512791025421fd4
Hemanth Nag Chopparapu <139227551+hemu312004@users.noreply.github.com>
2024-03-01 01:03:15 +0530

    Add files via upload

---
+++ DAY 27 OF 50 Binary-to-Gray/tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+module Binary_to_Gray_tb;
+       reg [3:0]b;
+       wire [3:0]g;
+       
+       Binary_to_Gray uut (b,g);
+       
+       initial begin
+       b=4'b0000;
+       #10   b=4'b0001;
+       #10   b=4'b0010;
+       #10   b=4'b0011;
+       #10   b=4'b0100;
+       #10   b=4'b0101;
+       #10   b=4'b0110;
+       #10   b=4'b0111;
+       #10   b=4'b1000;
+       #10   b=4'b1001;
+       #10   b=4'b1010;
+       #10   b=4'b1011;
+       #10   b=4'b1100;
+       #10   b=4'b1101;
+       #10   b=4'b1110;
+       #10   b=4'b1111;
+       end
+endmodule
